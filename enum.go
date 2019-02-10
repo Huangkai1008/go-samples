2e4107aded65ffcc4c784aa88170c418332af098
huangkai <18778335525@163.com>
2019-02-11 03:52:27 +0800

    go枚举

---
+++ enum.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	type Weapon = int
+
+	const (
+		Arrow Weapon = iota
+		Shuriken
+		SniperRifle
+		Rifle
+		Blower
+	)
+
+	// 输出枚举值
+	fmt.Println(Arrow, Shuriken, SniperRifle, Rifle, Blower)
+}
