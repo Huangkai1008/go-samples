b59b8d58fe8f6416b60f3e0068a7e4645a0439b1
huangkai <18778335525@163.com>
2019-06-22 23:00:27 +0800

    perf: gopl-ch1 优化fetch程序

--- gopl.io/ch1/fetch/fetch.go
+++ gopl.io/ch1/fetch/fetch.go
@@ -3,25 +3,32 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		// 假如URL参数缺失协议前缀，添加一个http://前缀
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 
-		b , err := ioutil.ReadAll(resp.Body)
-		_ = resp.Body.Close()
-		if err != nil {
+		// 使用io.Copy方法节省内存
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+
+		resp.Body.Close()
+		// 添加状态码
+		fmt.Println(resp.StatusCode)
 	}
 }
