adeb1794a87588171090900657a90a41976c7212
huangkai <18778335525@163.com>
2019-06-23 23:19:50 +0800

    feat: gopl-ch2 本地包的导入

---
+++ gopl.io/ch2/cf/main.go
@@ -0,0 +1,24 @@
+// cf把它的数值参数转换为摄氏温度和华氏温度
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"go-starter/gopl.io/ch2/tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n",
+			f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
--- gopl.io/ch2/tempconv/tempconv.go
+++ gopl.io/ch2/tempconv/tempconv.go
@@ -1,6 +1,8 @@
 // 包 tempconv 维护华氏温度和摄氏温度的基本信息
 package tempconv
 
+import "fmt"
+
 type Celsius float64
 type Fahrenheit float64
 
@@ -9,3 +11,11 @@ const (
 	FreeingC      Celsius = 0
 	BoilingC      Celsius = 100
 )
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%gC", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%gF", f)
+}
