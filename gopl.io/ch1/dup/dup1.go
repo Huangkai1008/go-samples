fb58a47118fce7c6b3f2c33ffc2604d297875414
huangkai <18778335525@163.com>
2019-06-25 23:37:13 +0800

    feat: gopl-ch3 切换换行符

--- gopl.io/ch1/dup/dup1.go
+++ gopl.io/ch1/dup/dup1.go
@@ -15,7 +15,7 @@ func main() {
 	}
 	// 注意: 忽略input.Err()中可能的错误
 	for line, n := range counts {
-		if n > 1{
+		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
--- gopl.io/ch1/dup/dup2.go
+++ gopl.io/ch1/dup/dup2.go
@@ -20,7 +20,7 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-				countLines(f, counts)
+			countLines(f, counts)
 			f.Close()
 		}
 	}
@@ -32,7 +32,7 @@ func main() {
 	}
 }
 
-func countLines(f *os.File, counts map[string]int){
+func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
