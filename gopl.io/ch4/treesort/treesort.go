e4753461fba80f281f37198b506c90aeecef2a1a
huangkai <18778335525@163.com>
2019-06-29 22:36:04 +0800

    feat: gopl-ch4 使用二叉树进行插入排序

---
+++ gopl.io/ch4/treesort/treesort.go
@@ -0,0 +1,40 @@
+// treesort使用二叉树实现插入排序
+package treesort
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// 就地排序
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues 将元素按照顺序追加到values中，然后返回结果slice
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
