22a1e7fbb65dc24b525730989e0a4df004d92e5f
huangkai <18778335525@163.com>
2019-07-06 23:32:26 +0800

    feat: gopl-ch5 outline2

---
+++ gopl.io/ch5/outline2/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		_ = outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	forEachNode(doc, startElement, endElement)
+	return nil
+}
+
+// forEachNode 调用 pre(x) 和 post(x) 遍历以n为根的树中的每个节点x
+// 两个函数是可选的
+// pre函数 在子节点被访问前（前序）调用
+// post函数 在子节点被访问后（后序）调用
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+	}
+}
