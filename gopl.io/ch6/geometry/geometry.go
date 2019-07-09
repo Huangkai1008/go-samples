d2439b704fbaa913886c0e7aea95a7002b750e50
huangkai <18778335525@163.com>
2019-07-09 23:33:05 +0800

    feat: gopl-ch6 方法声明

---
+++ gopl.io/ch6/geometry/geometry.go
@@ -0,0 +1,32 @@
+// 平面几何相关的包
+package geometry
+
+import "math"
+
+type Point struct {
+	X, Y float64
+}
+
+// 普通的函数
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// Point类型方法
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// Path 是连接多个点的直线段
+type Path []Point
+
+// Distance 方法返回路径的长度
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
