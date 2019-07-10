7a1d5f1de1c57f4b7ad8764e769fb827daf92ec4
huangkai <18778335525@163.com>
2019-07-10 16:12:40 +0800

    feat: gopl-ch6 IntSet使用位向量实现

---
+++ gopl.io/ch6/intset/inset_test.go
@@ -0,0 +1,22 @@
+package intset
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIntSet_One(t *testing.T) {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(9), x.Has(123))
+}
---
+++ gopl.io/ch6/intset/intset.go
@@ -0,0 +1,59 @@
+// 包intset基于位向量提供整数集合容器
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+// Has方法的返回值表示是否集合内是否存在非负整数x
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint64(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add方法添加非负整数x到集合中
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith方法将会对s和t做并集并将结果存在s中
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, word := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= word
+		} else {
+			s.words = append(s.words, word)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				_, _ = fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// TODO 定义IntSet额外方法
