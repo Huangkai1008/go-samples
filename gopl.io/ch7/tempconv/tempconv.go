42d57903feeba09323d00539d0b1583a306cac5e
huangkai <18778335525@163.com>
2019-07-12 00:04:15 +0800

    feat: gopl-ch7 使用flag.Value解析参数实现从命令行输出温度

---
+++ gopl.io/ch7/tempconv/tempconv.go
@@ -0,0 +1,47 @@
+// 包tempconv提供摄氏和华氏度互转
+package tempconv
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Celsius float64
+type Fahrenheit float64
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9.0/5.0 + 32.0)
+}
+
+func FtoC(f Fahrenheit) Celsius {
+	return Celsius((f - 32.0) * 5.0 / 9.0)
+}
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%gC", c)
+}
+
+type celsiusFlag struct {
+	Celsius
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	_, _ = fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F":
+		f.Celsius = FtoC(Fahrenheit(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
