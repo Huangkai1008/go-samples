3c44f83e00eed120b021f07b991b08b31d8bfaf9
huangkai <18778335525@163.com>
2019-07-11 23:47:22 +0800

    feat: gopl-ch7 实现了睡眠指定时间的功能

---
+++ gopl.io/ch7/sleep/main.go
@@ -0,0 +1,17 @@
+// sleep程序实现了睡眠指定时间的功能
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var period = flag.Duration("period", 1*time.Second, "sleep period")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...", *period)
+	time.Sleep(*period)
+	fmt.Println()
+}
