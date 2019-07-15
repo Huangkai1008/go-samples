e30b072a3e6243adab03d4cdd6682d9f20eaf578
huangkai <18778335525@163.com>
2019-07-15 23:38:06 +0800

    feat: gopl-ch7 Expr添加验证方法

--- gopl.io/ch7/eval/ast.go
+++ gopl.io/ch7/eval/ast.go
@@ -3,6 +3,8 @@ package eval
 type Expr interface {
 	// Eval 返回表达式在 env 上下文中的值
 	Eval(env Env) float64
+	// Check 方法报告表达式中的错误，并把表达式中的值加入Vars中
+	Check(vars map[Var]bool) error
 }
 
 // Var 表示一个变量，比如 x
