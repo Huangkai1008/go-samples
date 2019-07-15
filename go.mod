d1bc02b81de1a19e4f646b5a5ed022ad6817eca9
huangkai <18778335525@163.com>
2019-07-15 22:53:09 +0800

    feat: gopl-ch7 实现表达式求值器

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-starter
 
 go 1.12
 
-require golang.org/x/net v0.0.0-20200528225125-3c3fba18258b
+require (
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b
+	gopl.io v0.0.0-20200323155855-65c318dde95e // indirect
+)
---
+++ gopl.io/ch7/eval/ast.go
@@ -0,0 +1,30 @@
+package eval
+
+type Expr interface {
+	// Eval 返回表达式在 env 上下文中的值
+	Eval(env Env) float64
+}
+
+// Var 表示一个变量，比如 x
+type Var string
+
+// literal 是一个数字常量，比如 3.141
+type literal float64
+
+// unary 表示一元操作表达式，比如 -x
+type unary struct {
+	op rune // '+', '-'中的一个
+	x  Expr
+}
+
+// binary 表示二元操作表达式， 比如 x + y
+type binary struct {
+	op   rune // '+', '-', '*', '/'中的一个
+	x, y Expr
+}
+
+// call 表示函数调用表达式，比如 sin(x)
+type call struct {
+	fn   string // "pow", "sin", "sqrt"中的一个
+	args []Expr
+}
