a1f564705b0bdd5e5205cc58a66ddbd2422305b6
huangkai <18778335525@163.com>
2019-07-17 19:38:57 +0800

    feat: gopl-ch8 实现并发时钟服务器

---
+++ gopl.io/ch8/clock2/main.go
@@ -0,0 +1,36 @@
+// clock2 是一个定期报告时间的TCP服务器，支持多个客户端并发调用
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // 例如，连接终止
+			continue
+		}
+		go handleConn(conn)
+	}
+
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
