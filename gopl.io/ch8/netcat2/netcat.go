2dfa18e936603985bec519d2f989aad9dc0015c7
huangkai <18778335525@163.com>
2019-07-19 12:53:07 +0800

    feat: gopl-ch8 实现模拟回声服务器

---
+++ gopl.io/ch8/netcat2/netcat.go
@@ -0,0 +1,25 @@
+// netcat2 实现一个只读的TCP 客户端程序，支持接收终端输入
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	go mustCopy(os.Stdout, conn)
+	mustCopy(conn, os.Stdin)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ gopl.io/ch8/reverb1/reverb.go
@@ -0,0 +1,42 @@
+// reverb1 实现模拟回声的TCP服务器
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	_, _ = fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	_, _ = fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	_, _ = fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	_ = c.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
