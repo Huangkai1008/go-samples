aea91700f27dc3b395b4e4fd6e4cd0fb126e50b6
huangkai <18778335525@163.com>
2019-07-27 23:35:00 +0800

    feat: gopl-ch9 实现只有一个账户的并发银行

---
+++ gopl.io/ch9/bank1/bank.go
@@ -0,0 +1,28 @@
+// bank包提供了一个只有一个账户的并发安全银行
+package bank
+
+var deposits = make(chan int) // 发送存款额
+var balances = make(chan int) // 接收余额
+
+func Deposit(amount int) {
+	deposits <- amount
+}
+
+func Balance() int {
+	return <-balances
+}
+
+func teller() {
+	var balance int // balance被限制在teller goroutine中
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		}
+	}
+}
+
+func init() {
+	go teller() // 启动监控goroutine
+}
