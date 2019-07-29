5915c3733791ff326143286123fa5cfca9ba7928
huangkai <18778335525@163.com>
2019-07-29 23:01:55 +0800

    feat: gopl-ch9 实现对类型Func并发安全的函数记忆功能

---
+++ gopl.io/ch9/memo3/memo.go
@@ -0,0 +1,39 @@
+// 包memo提供了一个对类型Func并发安全的函数记忆功能
+// 对于不同的key的请求并发地运行
+// 对同一密钥的并发请求会导致重复的工作
+package memo
+
+import "sync"
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex
+	cache map[string]result
+}
+
+type Func func(string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	return &Memo{
+		f:     f,
+		cache: make(map[string]result),
+	}
+}
+
+func (memo *Memo) Get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	res, ok := memo.cache[key]
+	memo.mu.Unlock()
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.mu.Lock()
+		memo.cache[key] = res
+		memo.mu.Lock()
+	}
+	return res.value, res.err
+}
