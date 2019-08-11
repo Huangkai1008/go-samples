331d10abbdb53cb96470179464cef317dcb2299b
huangkai <18778335525@163.com>
2019-08-11 18:30:41 +0800

    feat(RabbitMQ tutorial): hello world

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-starter
 
 go 1.12
 
-require golang.org/x/net v0.0.0-20181220203305-927f97764cc3
+require (
+	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271 // indirect
+	golang.org/x/net v0.0.0-20181220203305-927f97764cc3
+)
---
+++ rabbitmq/send.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	queue, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World"
+	err = ch.Publish(
+		"",
+		queue.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body)},
+	)
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+}
