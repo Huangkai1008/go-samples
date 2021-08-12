aab2007524e3cc6ef59d0828e3d88b04e90a83d6
huangkai <h1770360848@outlook.com>
2021-08-12 18:35:17 +0800

    feat(tdd): contxt

---
+++ learn_go_with_tests/context/context_test.go
@@ -0,0 +1,43 @@
+package context
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		assert.Equal(t, data, response.Body.String())
+		assert.NotEqual(t, true, store.cancelled)
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+		svr.ServeHTTP(response, request)
+
+		assert.Equal(t, true, store.cancelled)
+	})
+}
